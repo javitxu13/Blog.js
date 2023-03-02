27fc667db2aedf3c714bd7a3e5c40421d1db7165
Javier <javiergonzalez13a@gmail.com>
2023-03-02 17:18:44 +0100

    guardar

--- blog.js
+++ blog.js
@@ -2,8 +2,8 @@
 //añadir listenner
 //conseguir valores del input
 //meter los valores en blog section
-// .reset para borrar todo a la vez
-//
+//.reset para borrar todo a la vez
+// 
 
 let form = document.getElementById("blog_form");
 form.addEventListener("submit", createPost);
@@ -14,23 +14,27 @@ form.addEventListener("submit", createPost);
         
         let h3 = document.createElement("h3");
         let p = document.createElement("p");
-
+    
         let textTitulo = document.getElementById("titulo").value;
         let textContenido = document.getElementById("contenido").value;
         let article = document.createElement("article");
-
-
+        let section = document.createElement("section")
         let icono = document.createElement ("i");
-        icono.classList.add("fa","fa-blind");
-        article.appendChild(icono);
+        let iconoarticle = document.createElement("i");
         article.addEventListener("click",clickImportant);
+        section.addEventListener("click",clickImportant);
 
 
         h3.innerText = textTitulo;
         p.innerText = textContenido;
 
+        icono.classList.add("fa","fa-blind");
+
         article.appendChild(h3);
         article.appendChild(p);
+        article.appendChild(icono);
+        section.appendChild(article);
+
         document.getElementById("blog_section").appendChild(article);
         document.getElementById("titulo").value="";  
         document.getElementById("contenido").value="";
@@ -39,9 +43,13 @@ form.addEventListener("submit", createPost);
             let parent = element.parentElement;
             let text = parent.innerText;
             if(confirm("¿Deseas borrar este producto? \n" + text)){
-
             parent.remove();
         }
+    
+    }
+    function deleteSection(){
+
+
     }
 
         
